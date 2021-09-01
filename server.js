984d096a719011f0393c4005b3f0ed1a29b0a121
Edgar Calderon <EdgarCalderon_24@hotmail.com>
2021-09-01 17:18:29 -0600

    updating files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-const uuid = require("../helper/helper")
+const uuid = require("./helper/helper")
 const {response} = require("express");
 
 
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 
 
-app.use(express.json());
 app.use(express.urlencoded({extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
 
